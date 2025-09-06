f0d9f1dc72a298f1662e9b1ffd9401b10ac305cf
Alysson Ribeiro <15274059+sonalys@users.noreply.github.com>
2025-09-06 20:49:55 +0200

    move http error handler to own file

---
+++ internal/infrastructure/http/router/errors.go
@@ -0,0 +1,119 @@
+package router
+
+import (
+	"context"
+	"errors"
+	"net/http"
+
+	"github.com/ogen-go/ogen/ogenerrors"
+	"github.com/ogen-go/ogen/validate"
+	v1 "github.com/sonalys/goshare/internal/application/v1"
+	"github.com/sonalys/goshare/internal/domain"
+	"github.com/sonalys/goshare/internal/infrastructure/http/server"
+)
+
+func (a *Router) NewError(ctx context.Context, err error) *server.ErrorResponseStatusCode {
+	if target := new(ogenerrors.SecurityError); errors.As(err, &target) {
+		return newErrorResponse(http.StatusUnauthorized, server.Error{
+			Code:    server.ErrorCodeUnauthorized,
+			Message: target.Err.Error(),
+		})
+	}
+
+	if errors.Is(err, v1.ErrForbidden) {
+		return newErrorResponse(http.StatusForbidden, server.Error{
+			Code:    server.ErrorCodeUnauthorized,
+			Message: "not authorized to access resource",
+		})
+	}
+
+	if target := new(validate.Error); errors.As(err, &target) {
+		errs := make([]server.Error, 0, len(target.Fields))
+
+		for _, fieldErr := range target.Fields {
+			errs = append(errs, server.Error{
+				Code:    server.ErrorCodeInvalidField,
+				Message: fieldErr.Error.Error(),
+				Metadata: server.NewOptErrorMetadata(server.ErrorMetadata{
+					Field: server.NewOptString(fieldErr.Name),
+				}),
+			})
+		}
+
+		return newErrorResponse(http.StatusBadRequest, errs...)
+	}
+
+	if target := new(domain.FieldErrors); errors.As(err, target) {
+		errs := make([]server.Error, 0, len(*target))
+
+		for _, fieldErr := range *target {
+			errs = append(errs, server.Error{
+				Code:    server.ErrorCodeInvalidField,
+				Message: fieldErr.Cause.Error(),
+				Metadata: server.NewOptErrorMetadata(server.ErrorMetadata{
+					Field: server.NewOptString(fieldErr.Field),
+				}),
+			})
+		}
+
+		return newErrorResponse(http.StatusBadRequest, errs...)
+	}
+
+	if errors.Is(err, v1.ErrNotFound) {
+		return newErrorResponse(http.StatusNotFound, server.Error{
+			Code:    server.ErrorCodeNotFound,
+			Message: err.Error(),
+		})
+	}
+
+	if target := new(domain.FieldError); errors.As(err, target) {
+		return newErrorResponse(http.StatusBadRequest, server.Error{
+			Code:    server.ErrorCodeInvalidField,
+			Message: target.Cause.Error(),
+			Metadata: server.NewOptErrorMetadata(server.ErrorMetadata{
+				Field: server.NewOptString(target.Field),
+			}),
+		})
+	}
+
+	if target := new(validate.InvalidContentTypeError); errors.As(err, &target) {
+		return newErrorResponse(http.StatusUnsupportedMediaType, server.Error{
+			Code:    server.ErrorCodeRequiredHeader,
+			Message: target.Error(),
+		})
+	}
+
+	if target := new(ogenerrors.DecodeParamError); errors.As(err, &target) {
+		return newErrorResponse(http.StatusBadRequest, server.Error{
+			Code:    server.ErrorCodeInvalidParameter,
+			Message: target.Err.Error(),
+			Metadata: server.NewOptErrorMetadata(server.ErrorMetadata{
+				Field: server.NewOptString(target.Name),
+			}),
+		})
+	}
+
+	if target := new(ogenerrors.DecodeBodyError); errors.As(err, &target) {
+		return newErrorResponse(http.StatusBadRequest, server.Error{
+			Code:    server.ErrorCodeInvalidField,
+			Message: target.Err.Error(),
+			Metadata: server.NewOptErrorMetadata(server.ErrorMetadata{
+				Field: server.NewOptString("body"),
+			}),
+		})
+	}
+
+	return newErrorResponse(http.StatusInternalServerError, server.Error{
+		Code:    server.ErrorCodeInternalError,
+		Message: "internal server error",
+	})
+}
+
+func newErrorResponse(statusCode int, errs ...server.Error) *server.ErrorResponseStatusCode {
+	return &server.ErrorResponseStatusCode{
+		StatusCode: statusCode,
+		Response: server.ErrorResponse{
+			Errors: errs,
+		},
+	}
+}
--- internal/infrastructure/http/router/router.go
+++ internal/infrastructure/http/router/router.go
@@ -1,24 +1,14 @@
 package router
 
 import (
-	"context"
-	"errors"
-	"net/http"
-
-	"github.com/google/uuid"
-	"github.com/ogen-go/ogen/ogenerrors"
-	"github.com/ogen-go/ogen/validate"
 	"github.com/sonalys/goshare/internal/application/controllers/identitycontroller"
 	"github.com/sonalys/goshare/internal/application/controllers/usercontroller"
-	v1 "github.com/sonalys/goshare/internal/application/v1"
-	"github.com/sonalys/goshare/internal/domain"
 	"github.com/sonalys/goshare/internal/infrastructure/http/router/expenses"
 	"github.com/sonalys/goshare/internal/infrastructure/http/router/ledgers"
 	"github.com/sonalys/goshare/internal/infrastructure/http/router/records"
 	"github.com/sonalys/goshare/internal/infrastructure/http/router/users"
 	"github.com/sonalys/goshare/internal/infrastructure/http/server"
 	"github.com/sonalys/goshare/internal/ports"
-	"go.opentelemetry.io/otel/trace"
 )
 
 type (
@@ -40,132 +30,5 @@ func New(
 		UsersHandler:    users.New(securityHandler, identityController, userController),
 		ExpensesHandler: expenses.New(securityHandler, userController),
 		RecordsHandler:  records.New(securityHandler, userController),
-	}
-}
-
-func newErrorResponse(ctx context.Context, statusCode int, errs ...server.Error) *server.ErrorResponseStatusCode {
-	var traceID trace.TraceID
-	if span := trace.SpanFromContext(ctx); span != nil {
-		traceID = span.SpanContext().TraceID()
-	}
-
-	return &server.ErrorResponseStatusCode{
-		StatusCode: statusCode,
-		Response: server.ErrorResponse{
-			TraceID: uuid.UUID(traceID),
-			Errors:  errs,
-		},
-	}
-}
-
-func (a *Router) NewError(ctx context.Context, err error) *server.ErrorResponseStatusCode {
-	if target := new(ogenerrors.SecurityError); errors.As(err, &target) {
-		return newErrorResponse(ctx, http.StatusUnauthorized, server.Error{
-			Code:    server.ErrorCodeUnauthorized,
-			Message: target.Err.Error(),
-		})
-	}
-
-	if errors.Is(err, v1.ErrForbidden) {
-		return newErrorResponse(ctx, http.StatusForbidden, server.Error{
-			Code:    server.ErrorCodeUnauthorized,
-			Message: "not authorized to access resource",
-		})
-	}
-
-	if target := new(validate.Error); errors.As(err, &target) {
-		errs := make([]server.Error, 0, len(target.Fields))
-
-		for _, fieldErr := range target.Fields {
-			errs = append(errs, server.Error{
-				Code:    server.ErrorCodeInvalidField,
-				Message: fieldErr.Error.Error(),
-				Metadata: server.NewOptErrorMetadata(server.ErrorMetadata{
-					Field: server.NewOptString(fieldErr.Name),
-				}),
-			})
-		}
-
-		return newErrorResponse(ctx, http.StatusBadRequest, errs...)
-	}
-
-	if target := new(domain.FieldErrors); errors.As(err, target) {
-		errs := make([]server.Error, 0, len(*target))
-
-		for _, fieldErr := range *target {
-			errs = append(errs, server.Error{
-				Code:    server.ErrorCodeInvalidField,
-				Message: fieldErr.Cause.Error(),
-				Metadata: server.NewOptErrorMetadata(server.ErrorMetadata{
-					Field: server.NewOptString(fieldErr.Field),
-				}),
-			})
-		}
-
-		return newErrorResponse(ctx, http.StatusBadRequest, errs...)
-	}
-
-	if errors.Is(err, v1.ErrNotFound) {
-		return newErrorResponse(ctx, http.StatusNotFound, server.Error{
-			Code:    server.ErrorCodeNotFound,
-			Message: err.Error(),
-		})
-	}
-
-	if target := new(domain.FieldError); errors.As(err, target) {
-		return newErrorResponse(ctx, http.StatusBadRequest, server.Error{
-			Code:    server.ErrorCodeInvalidField,
-			Message: target.Cause.Error(),
-			Metadata: server.NewOptErrorMetadata(server.ErrorMetadata{
-				Field: server.NewOptString(target.Field),
-			}),
-		})
-	}
-
-	if target := new(validate.InvalidContentTypeError); errors.As(err, &target) {
-		return newErrorResponse(ctx, http.StatusUnsupportedMediaType, server.Error{
-			Code:    server.ErrorCodeRequiredHeader,
-			Message: target.Error(),
-		})
-	}
-
-	if target := new(ogenerrors.DecodeParamError); errors.As(err, &target) {
-		return newErrorResponse(ctx, http.StatusBadRequest, server.Error{
-			Code:    server.ErrorCodeInvalidParameter,
-			Message: target.Err.Error(),
-			Metadata: server.NewOptErrorMetadata(server.ErrorMetadata{
-				Field: server.NewOptString(target.Name),
-			}),
-		})
-	}
-
-	if target := new(ogenerrors.DecodeBodyError); errors.As(err, &target) {
-		return newErrorResponse(ctx, http.StatusBadRequest, server.Error{
-			Code:    server.ErrorCodeInvalidField,
-			Message: target.Err.Error(),
-			Metadata: server.NewOptErrorMetadata(server.ErrorMetadata{
-				Field: server.NewOptString("body"),
-			}),
-		})
-	}
-
-	return newErrorResponseWithStatusCode(ctx, http.StatusInternalServerError, server.Error{
-		Code:    server.ErrorCodeInternalError,
-		Message: "internal server error",
-	})
-}
-
-func newErrorResponseWithStatusCode(ctx context.Context, statusCode int, errs ...server.Error) *server.ErrorResponseStatusCode {
-	var traceID trace.TraceID
-	if span := trace.SpanFromContext(ctx); span != nil {
-		traceID = span.SpanContext().TraceID()
-	}
-
-	return &server.ErrorResponseStatusCode{
-		StatusCode: statusCode,
-		Response: server.ErrorResponse{
-			TraceID: uuid.UUID(traceID),
-			Errors:  errs,
-		},
 	}
 }
